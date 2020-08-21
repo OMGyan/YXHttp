9df33ee8d3c66e6e02c8c27f408f97e3d371b066
yanxun <1768695841@qq.com>
2020-08-21 17:13:54 +0800

    示例更新

--- app/src/main/java/com/yx/yxhttp/sample/MainActivity.java
+++ app/src/main/java/com/yx/yxhttp/sample/MainActivity.java
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.yx.yxhttp.IJsonDataTransForm;
 import com.yx.yxhttp.YXHttp;
 
@@ -14,47 +16,36 @@ import java.util.Map;
 import static com.yx.yxhttp.YXHttp.TAG;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageView iv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        iv = findViewById(R.id.iv);
     }
 
     public void goRequest(View view) {
-        /**
-         * POST请求示例
-         */
-        Map<String, Object> map2 = new LinkedHashMap<>();
-        map2.put("who","风不会停息1029");
-        map2.put("type","Android");
-        map2.put("desc","Android中NFC标签卡的读取");
-        map2.put("url","https://blog.csdn.net/qq_35189116/article/details/80454677");
-        map2.put("debug",false);
 
-        YXHttp.sendJsonRequest("https://gank.io/api/add2gank",map2,false,"POST", bean1.class, new IJsonDataTransForm<bean1>() {
-            @Override
-            public void onSuccess(bean1 m) {
-                 Log.i(TAG, "onSuccess: "+m.getMsg());
-            }
-            @Override
-            public void onFailure(Throwable throwable) {
-                Log.i(TAG, "onFailure: "+throwable.toString());
-            }
-        });
+//        Map<String, Object> map2 = new LinkedHashMap<>();
+//        map2.put("who","风不会停息1029");
+//        map2.put("type","Android");
+//        map2.put("desc","Android中NFC标签卡的读取");
+//        map2.put("url","https://blog.csdn.net/qq_35189116/article/details/80454677");
+//        map2.put("debug",false);
 
         /**
          * Get请求示例
          */
-        /*YXHttp.sendJsonRequest("http:cxxx",null,false,null,bean2.class, new IJsonDataTransForm<bean2>() {
+        YXHttp.sendJsonRequest("https://gank.io/api/v2/data/category/Girl/type/Girl/page/1/count/10",null,
+                false,"GET",bean1.class, new IJsonDataTransForm<bean1>() {
             @Override
-            public void onSuccess(bean2 m) {
-                Log.i(TAG, "onSuccess: "+m.getResults().get(0).toString());
+            public void onSuccess(bean1 m) {
+
+                Glide.with(MainActivity.this).load(m.getData().get(1).getUrl()).into(iv);
+
             }
             @Override
-            public void onFailure(Throwable throwable) {
-                Log.i(TAG, "onFailure: "+throwable.toString());
-            }
-        });*/
+            public void onFailure(Throwable throwable) { }
+        });
     }
 }
